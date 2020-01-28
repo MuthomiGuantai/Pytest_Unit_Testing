c5b46550eabc044dc710db0b7266159b7c5a11f6
Bruce Muthomi Guantai <muthomiguantai@gmail.com>
2020-01-28 12:19:09 +0300

    setup & tear down,fixtures

--- test_math_func.py
+++ test_math_func.py
@@ -1,5 +1,6 @@
 import math_func
 import pytest
+from math_func import StudentDB
 
 
 @pytest.mark.number
@@ -31,3 +32,33 @@ def test_product_strings():
     assert result == 'H H '
     assert type(result) is str
     assert 'H' in result
+
+@pytest.mark.parametrize('num1, num2, result',[
+    (7, 3, 10),
+    ('Hello', 'World', 'HelloWorld'),
+    (10.5, 25.5, 36)
+])
+def test_add_All(num1, num2, result):
+    assert math_func.add(num1, num2) == result
+
+@pytest.fixture(scope='module')
+def db():
+    print('----------setup----------------')
+    db = StudentDB()
+    db.connect('data.json')
+    yield db
+    print('----------teardown----------------')
+    db.close()
+
+def test_scott_data(db):
+    scott_data = db.get_data('Scott')
+    assert scott_data['id'] == 1
+    assert scott_data['name'] == 'Scott'
+    assert scott_data['result'] == 'pass'
+
+def test_mark_data(db):
+    mark_data = db.get_data('Mark')
+    assert mark_data['id'] == 2
+    assert mark_data['name'] == 'Mark'
+    assert mark_data['result'] == 'fail'
+  
